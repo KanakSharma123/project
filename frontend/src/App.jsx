041348a1bbbd9dbe75431ee87873227e1c668457
Kanak Sharma <kanaksharma0512.ks@gmail.com>
2026-05-22 15:06:08 +0530

    Fix Tailwind CSS setup and frontend configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Dashboard from "./pages/Dashboard";
-import NewProject from "./pages/NewProject";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/new-project" element={<NewProject />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="bg-black text-red-500 text-6xl p-10">
+      Tailwind Working
+    </div>
   );
 }
 
